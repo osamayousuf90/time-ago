91687ecb4978c74f2924d6259437d09bc05c5448
Osama Yousuf <94833503+osamayousuf90@users.noreply.github.com>
2023-01-23 15:36:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,29 @@ export default function App() {
     </div>
   );
 }
+
+
+
+
+// You can use the react-time-ago library to display the relative time for dates that are within 3 days of the current date, and then use the toLocaleString() method of the Date object to display the original format date for dates that are older than 3 days.
+// Here is an example of how you can implement this in a React component:
+
+import TimeAgo from 'react-time-ago'
+
+function MyComponent(props) {
+  const { time } = props;
+  const threeDays = 3 * 24 * 60 * 60 * 1000;
+  const currentTime = new Date().getTime();
+  const postedTime = new Date(time).getTime();
+  const timeDiff = currentTime - postedTime;
+
+  if (timeDiff < threeDays) {
+    return <TimeAgo date={time} />;
+  } else {
+    return <span>{new Date(time).toLocaleString()}</span>;
+  }
+}
+
+
+// This will display the relative time using react-time-ago for dates that are within 3 days of the current date, and will display the original format date using toLocaleString() for dates that are older than 3 days.
+// You can adjust the value of threeDays to suit your needs, it's representing time in milliseconds.
